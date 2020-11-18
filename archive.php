a5a1773e00f732dfe40f5ec906a7df1ab71f910f
Vladimir Samojlov <v-moi@ya.ru>
2020-11-18 22:32:07 +0200

    edit

--- archive.php
+++ archive.php
@@ -22,26 +22,7 @@
 	<div class="container mx-auto px-2 lg:px-0 mb-10">
 		<div class="w-full lg:w-9/12 mx-auto">
 			<div>
-				<?php 
-				$current_term = get_queried_object_id();
-				$current_page = !empty( $_GET['page'] ) ? $_GET['page'] : 1;
-				$custom_query = new WP_Query( array( 
-					'post_type' => 'post', 
-					'posts_per_page' => 10,
-					'paged' => $current_page,
-					'orderby' => 'date',
-					'order' => 'DESC',
-					'tax_query' => array(
-				    array(
-			        'taxonomy' => 'category',
-					    'terms' => $current_term,
-			        'field' => 'term_id',
-			        'include_children' => true,
-			        'operator' => 'IN'
-				    )
-					),
-				));
-				if ($custom_query->have_posts()) : while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					<a href="<?php the_permalink(); ?>" class="block mb-10 lg:mb-6">
 						<div class="flex items-start lg:items-center">
 							<img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'thumbnail') ?>" alt="<?php the_title(); ?>	" loading="lazy" class="rounded-full object-cover mr-4" width="80px" height="80px">
