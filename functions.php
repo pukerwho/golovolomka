dda048ff8553308b82eec4608f72e598688ce04f
Vladimir Samojlov <v-moi@ya.ru>
2020-11-23 13:02:28 +0200

    ediit

--- functions.php
+++ functions.php
@@ -57,6 +57,7 @@ function load_admin_styles() {
 remove_action( 'wp_head', '_wp_render_title_tag', 1 );
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
+remove_filter('the_content', 'wptexturize');
 
 add_action('do_feed', 'itsme_disable_feed', 1);
 add_action('do_feed_rdf', 'itsme_disable_feed', 1);
