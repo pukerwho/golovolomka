a570f4d2ab497b1eabb12f878232e32599071eea
Vladimir Samojlov <v-moi@ya.ru>
2020-12-01 21:53:12 +0200

    edit

--- single.php
+++ single.php
@@ -25,7 +25,7 @@
 												<meta itemprop="position" content="1">
 											</li>
 											<li itemprop='itemListElement' itemscope itemtype='http://schema.org/ListItem' class="breadcrumbs-item">
-												<a itemprop="item" href="<?php get_post_type_archive_link('post'); ?>" class="breadcrumbs-link text-sm">
+												<a itemprop="item" href="<?php get_post_type_archive_link('posts'); ?>" class="breadcrumbs-link text-sm">
 													<span itemprop="name"><?php _e('Публикации', 'totop'); ?></span>
 												</a>                        
 												<meta itemprop="position" content="2">
