9fc5bb72faf3b01db623b7f640eff63a9cb2870b
Vladimir Samojlov <v-moi@ya.ru>
2020-12-21 07:44:18 +0200

    edit

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-  <title><?php echo $title.' — '.$siteName; ?></title>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
