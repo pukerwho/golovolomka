a3a38e426039e98e364c1b08446550bc726e8ba3
Vladimir Samojlov <v-moi@ya.ru>
2021-01-04 10:34:20 +0200

    edit

--- functions.php
+++ functions.php
@@ -71,6 +71,7 @@ remove_action( 'wp_head', 'wp_shortlink_wp_head');
 remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
 remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
 remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+remove_action( 'wp_head', 'wlwmanifest_link');
 remove_action('rest_api_init', 'wp_oembed_register_route');
 remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
 remove_action('wp_print_styles', 'print_emoji_styles');
