9875f795153452d4d6a2ac1c3c4cc7e9a927a7a1
Vladimir Samojlov <v-moi@ya.ru>
2021-01-28 08:12:37 +0200

    edit

--- header.php
+++ header.php
@@ -18,15 +18,7 @@
   <?php
     wp_head();
   ?>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127554563-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-127554563-1');
-  </script>
+  
 
 </head>
 <body <?php echo body_class(); ?>>
