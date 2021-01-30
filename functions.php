86ec66ead9c16c5bade3fd6634e83d1d90dab9b7
Vladimir Samojlov <v-moi@ya.ru>
2021-01-30 13:29:36 +0200

    edit

--- functions.php
+++ functions.php
@@ -77,13 +77,6 @@ remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_filter('the_content', 'wptexturize');
 
-add_action('do_feed', 'itsme_disable_feed', 1);
-add_action('do_feed_rdf', 'itsme_disable_feed', 1);
-add_action('do_feed_rss', 'itsme_disable_feed', 1);
-add_action('do_feed_rss2', 'itsme_disable_feed', 1);
-add_action('do_feed_atom', 'itsme_disable_feed', 1);
-add_action('do_feed_rss2_comments', 'itsme_disable_feed', 1);
-add_action('do_feed_atom_comments', 'itsme_disable_feed', 1);
 remove_action( 'wp_head', 'feed_links_extra', 3 );
 remove_action( 'wp_head', 'feed_links', 2 );
 
